6157610dc04c161586e11488e8114639b121a5e0
Siva <siva.sivanesan@perficient.com>
2023-11-02 01:34:05 +0530

    added config changes in cloud.cfg file

--- sshd_config.sh
+++ sshd_config.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo sed -i 's/lock_passwd: true/lock_passwd: false/' /etc/cloud/cloud.cfg
+sudo sed -i 's/ssh_pwauth:   0/ssh_pwauth:   True/' /etc/cloud/cloud.cfg
+
 # Set a password for ec2-user
 echo "ansible" | sudo passwd --stdin ec2-user
 
