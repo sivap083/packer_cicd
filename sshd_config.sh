57aa1546165848d520c1ceff98dfc2bce3009cf2
Siva <siva.sivanesan@perficient.com>
2023-11-02 02:26:17 +0530

    added config changes

--- sshd_config.sh
+++ sshd_config.sh
@@ -6,9 +6,10 @@ sudo sed -i 's/lock_passwd: true/lock_passwd: false/' /etc/cloud/cloud.cfg
 sudo sed -i 's/ssh_pwauth:   0/ssh_pwauth:   True/' /etc/cloud/cloud.cfg
 
 # Set a password for ec2-user
-echo "ansible" | sudo passwd --stdin ec2-user
+echo "Duod1pcdABe5P" | sudo passwd --stdin ec2-user
 
 # Enable password-based authentication in sshd_config
+echo "Updating ssh config"
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 
 # Restart SSH service
