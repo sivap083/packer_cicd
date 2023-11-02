3ddda2c6efa31a4fe975e312c346175619fdea08
Siva <siva.sivanesan@perficient.com>
2023-11-03 02:03:12 +0530

    added dnf repository

--- ansible-install.sh
+++ ansible-install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set +e
 # Update the package repository and install EPEL repository
+sudo dnf install epel-release -y
+sudo dnf update -y
 sudo yum update -y
-sudo amazon-linux-extras install epel
 sudo yum makecache
-sudo yum install -y epel-release
 
 # Install Ansible
-sudo yum install -y ansible
+sudo dnf install ansible
